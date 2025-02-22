489fbdb28f03bccd538de9dc119752226527fa3e
Bálint Dózsa <balint.dozsa20@gmail.com>
2025-02-22 18:24:44 +0100

    Fix when termekek is empty

--- weboldal/src/components/Termekek.jsx
+++ weboldal/src/components/Termekek.jsx
@@ -61,7 +61,7 @@ function Gallery() {
                 </div>
             </div>
         <div className='w3-row-padding'>
-            {termekek.map((termek)=>
+            {termekek?.map((termek)=>
             <div key={termek.id} className='w3-half w3-margin-bottom'>
                 <h2>{termek.nev}</h2>
                 <p>{termek.leiras}</p>
